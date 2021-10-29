0e6c39d60377a3ed20efa5ae4eb01f668c114427
vanevskidarko <vanevskidarko@gmail.com>
2021-10-29 17:26:25 +0200

    Vtora Auditoriska Vezba

---
+++ Auditoriska vezba 2/zadaca1/main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    prva zadaca
+    printf("Hello world!\n");
+    float x = 3.0/2 + (5-46*5.0/12);
+    printf("x = %.2f\n", x);
+    */
+
+
+    /*
+    Vtora zadaca
+    int x = 7;
+    printf("Brojot %d na treta e %d\n",x, x*x*x);
+    */
+
+    /*
+    int a = 3;
+    int b = 8;
+    int c = 12;
+    float d = (a+b+c) / 3.0;
+    printf("aritmeticka sredina e %.2f\n", d);
+    */
+
+    /*
+    Modul
+
+    int a = 19;
+    printf("Ostatok na 19 pri delenje so 2 e %d\n", a%2);
+    printf("Ostatok na 19 pri delenje so 3 e %d\n", a%3);
+    printf("Ostatok na 19 pri delenje so 5 e %d\n", a%5);
+    printf("Ostatok na 19 pri delenje so 8 e %d\n", a%8);
+    */
+
+    /*
+    perimetar plostina na krug
+
+    const float p = 3.141;
+    float rad;
+    float perimetar = 0;
+    float plostina = 0;
+    printf("Vnesete go radius na krugot \n");
+    scanf("%f", &rad);
+    perimetar = 2*rad*p;
+    plostina = rad*rad*p;
+
+    printf("P  = %f\n", perimetar);
+    printf("L = %f\n", plostina);
+    */
+
+
+    /* Konverzija na golema bukva vo mala bukva
+    char character;
+
+    printf("Vnesete golema bukva \n");
+    scanf("%c", &character);
+    printf("'%c malo se pisuva kako : '%c'\n", character, character+32);
+    */
+
+    /*
+    Temp Conversion
+
+    float C,F;
+    scanf("%f",&C);
+    F = 1.8 * C + 32.0;
+    printf("pretvorenite %.2f  vo celziusovi e %.2f farenhajtovi ", C, F);
+    */
+
+    return 0;
+}
